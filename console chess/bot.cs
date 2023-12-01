29414f2d949eaf02c367d9a50f5530da30f24314
SkyeTheCatgirl <lolbeans.b@gmail.com>
2023-12-01 11:15:50 +0000

    move validatoin fixes

--- console chess/bot.cs
+++ console chess/bot.cs
@@ -36,74 +36,91 @@ namespace console_chess
         private List<byte[]> allMoveValidations(object[] board) //runs move validation for every position on the board and adds it to a list
         {
             List<byte[]> list = new List<byte[]>();
-            byte[] aaarg = new byte[2];
+            byte[] aaarg = new byte[3];
             for (int i = 0; i < 64; i++)
             {
-                //allmoves.Add(Globals.validateMoves(board[i]));
+                //aaarg[0] is the position of the piece
+                //aaarg[1] is the position of the place the piece wants to go to
+                //aaarg[2] is the data of the piece
                 
                 Globals.parameters[0] = i;
                 Globals.parameters[1] = board;
-                foreach (var item in Globals.validateMoves(board[i]))
+                List<int> validmoves = Globals.validateMoves(board[i]);
+                for (int j = 0; j < validmoves.Count();j++)
                 {
-                    //allmoves.Add(item);
-                    aaarg[0] = convStringToByteArray(Globals.mDid(board[i]) + i + Globals.mDside(board[i]));
-                    aaarg[1] = convStringToByteArray(Globals.mDid(board[i]) + item.ToString() + Globals.mDside(board[i]));
-                    list.Add(aaarg);
+                    int item = validmoves[j];
+                    if (Globals.mDside(board[i]) != Globals.playerColour)
+                    {
+                        //allmoves.Add(item);
+                        //aaarg[0] = convStringToByteArray(Globals.mDid(board[i]) + i + Globals.mDside(board[i])); //position of piece
+                        //convert string to byte array( id of the piece + number + side of the piece )
+                        aaarg[0] = Convert.ToByte(i);
+                        //aaarg[1] = convStringToByteArray(Globals.mDid(board[i]) + item.ToString() + Globals.mDside(board[i])); //destination of piece
+                        aaarg[1] = Convert.ToByte(item);
+                        aaarg[2] = convStringToByteArray(Globals.mDside(board[i]).ToString() + Globals.mDside(board[item]).ToString());
+                        list.Add(aaarg);
+                    }
                 }
             }
             return list;
         }
         private byte convStringToByteArray(string str)
         {
+            //REPUROSED now it splits the byte into two nibbles, the lhs stores data about the original piece, the rhs stores data about the destination
+
             //each byte stores both side and type of piece
             //e.g. black horsey would be 0001 0011, white bishop would be 0000 0100
             byte tmp = 0;
-            switch (str.Substring(0, 1))
-            {
-                default:break;
-                //case "empty": tmp += 0; break;
-                case "p": tmp += 1; break;
-                case "r": tmp += 2; break;
-                case "n": tmp += 3; break;
-                case "b": tmp += 4; break; 
-                case "q": tmp += 5; break;
-                case "k": tmp += 6; break;
-                //case "knook": tmp += 7; break;
-                //case "knishop": tmp += 8; break;
-            }
-            if (str.EndsWith("2")) //black
+            // switch (str.Substring(0, 1).ToLower())
+            // {
+            //     default:break;
+            //     //case "empty": tmp += 0; break;
+            //     case "p": tmp += 1; break;
+            //     case "r": tmp += 2; break;
+            //     case "n": tmp += 3; break;
+            //     case "b": tmp += 4; break; 
+            //     case "q": tmp += 5; break;
+            //     case "k": tmp += 6; break;
+            //     //case "knook": tmp += 7; break;
+            //     //case "knishop": tmp += 8; break;
+            // }
+            
+            //lhs
+            if (str.Substring(0,1) == "2") //black
             {
                 tmp += 16;
             }
-            else if (str.EndsWith("0")) //empty
+
+            //rhs
+            if (str.EndsWith("2"))
             {
-                tmp += 128;
+                tmp += 1;
+            }
+            else if (str.EndsWith("0"))
+            {
+                tmp += 8;
             }
 
             return tmp;
         }
         private object[] generateNewBoard(byte[] byteArray)
         {
-            object[] board = Globals.initBoard();
+            object[] board = Globals.board;
 
             board[byteArray[1]] = board[byteArray[0]];
             board[byteArray[0]] = null;
 
-            return null;
+            return board;
         }
         private byte[] assignValue(byte[] origMove)
         {
-            object[] board = Globals.initBoard();
-            byte[] moveWAttValue = new byte[] {origMove[0], origMove[1], };
+            object[] board = Globals.board;
+            byte[] moveWAttValue = new byte[] {origMove[0], origMove[1], origMove[2], };
             
             //move with attached value
 
             //if location is occupied and the piece occupied is a different side
-            if (origMove[1] < 128 && (origMove[1] & 16) != (origMove[0] & 16))
-            //Explaination:
-            //origMove[1] is the location of the piece
-            // < 128 means is the location empty or not
-            // & 16 is a bitwise AND operator to check what side the piece is on
+            if (origMove[2] < 8 && (origMove[2] == 16 | origMove[2] == 1))
             {
                 moveWAttValue[3] = Convert.ToByte(Globals.mDvalue(origMove[1]));
             }
