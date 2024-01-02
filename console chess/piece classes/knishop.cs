1e0c4bbfd084a01986e3740e4dd4c9b3794eb135
SkyeTheCatgirl <lolbeans.b@gmail.com>
2024-01-02 11:45:05 +0000

    Added knook and knishop

--- console chess/piece classes/knishop.cs
+++ console chess/piece classes/knishop.cs
@@ -2,8 +2,17 @@ using console_chess;
 
 public class knishop : Board
 {
-    public knishop(int input, object[] board)
+    public knishop(int input)
     {
+        value = 6;
+        name = "Knishop";
+        identifier = "I";
         side = input;
+    }
+    public List<int> validateknishop(int square, object[] board)
+    {
+        knight knight = new knight(side);
+        bishop bishop = new bishop(side);
+        return bishop.validatebishop(square, board).Concat(knight.validateknight(square, board)).ToList();
     }
 }
--- console chess/piece classes/knook.cs
+++ console chess/piece classes/knook.cs
@@ -2,8 +2,17 @@ using console_chess;
 
 public class knook : Board
 {
-    public knook(int input, object[] board)
+    public knook(int input)
     {
+        value = 7;
+        name = "Knook";
+        identifier = "O";
         side = input;
+    }
+    public List<int> validateknook(int square, object[] board)
+    {
+        knight knight = new knight(side);
+        rook rook = new rook(side);
+        return rook.validaterook(square, board).Concat(knight.validateknight(square, board)).ToList();
     }
 }
